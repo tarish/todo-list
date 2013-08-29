df84d0a125ec72c2e3c07c8c8e9360cd6bce7024
tarish <tarish@gmail.com>
2013-08-29 01:08:41 -0400

    Pushed the css and JavaScript into external files. Server is currently accessing the files with a hardcoded name that needs to be made more generic.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,29 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 http.createServer(function (request, response) {
+	var currentUrl = url.parse(request.url, true).href.toString();
+	console.log(currentUrl);
+
+	if(currentUrl.indexOf('.css') != -1) {
+		fs.readFile('./public/css/styles.css', function(error, data) {
+			if(error) { throw error; }
+			response.writeHead(200, { 'Content-Type': 'text/css' });
+			response.write(data);
+		    response.end();
+		});
+	}
+
+	if(currentUrl.indexOf('.js') != -1) {
+		fs.readFile('./public/js/helloWorld.js', function(error, data) {
+			if(error) { throw error; }
+			response.writeHead(200, { 'Content-Type': 'text/javascript' });
+			response.write(data);
+		    response.end();
+		});
+	}
+
 	fs.readFile('./public/index.html', function(error, data) {
 		if(error) { throw error; }
 		response.writeHead(200, { 'Content-Type': 'text/html' });
